9a9174ec15a1c0d2c9e7ccf48749ac50f963f115
ewnmm8 <54524772+ewnmm8@users.noreply.github.com>
2021-05-23 17:53:56 +0900

    禁止ワードの変更

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const blackword = ["_____", "_moved", "tab_now", "tab_list"]
+const blackword = ["_____", "_moved", "_tab_now", "_tab_list"]
 
 // 旧仕様から移行
 if (localStorage.getItem("_moved") === null) {
@@ -9,9 +9,9 @@ if (localStorage.getItem("_moved") === null) {
 
 // 汎用関数
 const save = function () {
-  localStorage.setItem(tab_now, memo_area.value)
-  localStorage.setItem("tab_now", tab_now)
-  localStorage.setItem("tab_list", tab_list.join(','))
+  localStorage.setItem(_tab_now, memo_area.value)
+  localStorage.setItem("_tab_now", _tab_now)
+  localStorage.setItem("_tab_list", _tab_list.join(','))
 }
 const newOption = function (op_name) {
   const option = document.createElement("option")
@@ -22,21 +22,21 @@ const newOption = function (op_name) {
 // 変数初期化
 const memo_area = document.getElementById("memo-area")
 const select = document.getElementById("tab")
-let tab_now = localStorage.getItem("tab_now") || "_____"
-let tab_list = localStorage.getItem("tab_list") || "_____"
-tab_list = tab_list.split(',') //配列化
+let _tab_now = localStorage.getItem("_tab_now") || "_____"
+let _tab_list = localStorage.getItem("_tab_list") || "_____"
+_tab_list = _tab_list.split(',') //配列化
 const text_add = document.getElementById("text_add")
 text_add.value = ""
 
 // タブのプルダウンを初期化
-for (const tab_name of tab_list) {
+for (const tab_name of _tab_list) {
   if (tab_name == "_____") continue
   newOption(tab_name)
 }
 
 // 画面初期化
-select.value = tab_now
-memo_area.value = localStorage.getItem(tab_now) || ""
+select.value = _tab_now
+memo_area.value = localStorage.getItem(_tab_now) || ""
 save()
 
 
@@ -48,8 +48,8 @@ memo_area.onchange = save
 
 // タブ変更時
 select.onchange = function () {
-  tab_now = select.value
-  memo_area.value = localStorage.getItem(tab_now) || ""
+  _tab_now = select.value
+  memo_area.value = localStorage.getItem(_tab_now) || ""
   save()
 }
 
@@ -59,14 +59,14 @@ document.getElementById("button_add").onclick = function () {
     alert("使用できないワードです。")
     return false
   }
-  if (tab_list.includes(text_add.value)) {
+  if (_tab_list.includes(text_add.value)) {
     alert("重複しています。")
     return false
   }
   if (text_add.value == "") return false
   newOption(text_add.value)
-  tab_list.push(text_add.value)
-  tab_now = text_add.value
+  _tab_list.push(text_add.value)
+  _tab_now = text_add.value
   select.value = text_add.value
   text_add.value = ""
   memo_area.value = ""
@@ -83,16 +83,16 @@ document.getElementById("button_delete").onclick = function () {
   if (!confirm("このタブを削除します。よろしいですか？")) {
     return false
   }
-  if (tab_now == "_____") {
+  if (_tab_now == "_____") {
     memo_area.value = ""
   }
   else {
-    const index = tab_list.indexOf(tab_now)
-    localStorage.removeItem(tab_now)
-    tab_list.splice(index, 1)
+    const index = _tab_list.indexOf(_tab_now)
+    localStorage.removeItem(_tab_now)
+    _tab_list.splice(index, 1)
     select.removeChild(select.getElementsByTagName("option")[index])
-    tab_now = select.value
-    memo_area.value = localStorage.getItem(tab_now) || ""
+    _tab_now = select.value
+    memo_area.value = localStorage.getItem(_tab_now) || ""
   }
   save()
 }
